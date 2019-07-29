392f4e0e16401bf89daf59048c3ed58fb4d70c7c
Karthik <krajendran@fasttrack.com.au>
2019-07-29 17:36:48 +1000

    Adding datatable to prime v8
    Adding datatable to prime v8 and rebasing imports

--- primeng.d.ts
+++ primeng.d.ts
@@ -19,6 +19,7 @@ export * from './components/contextmenu/contextmenu';
 export * from './components/datagrid/datagrid';
 export * from './components/datalist/datalist';
 export * from './components/datascroller/datascroller';
+export * from './components/datatable/datatable';
 export * from './components/defer/defer';
 export * from './components/dialog/dialog';
 export * from './components/dragdrop/dragdrop';
